2bca1df7642105b63d5fb2e19271ead2dc46c276
mohamedsaid-sd <sudanit2015@gmail.com>
2024-03-12 01:17:11 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,80 @@
+<header>
+<div id="header-top">
+	
+	<div class="row">
+		<div class="col-lg-1">
+			العربية
+		</div>
+
+		<div class="col-lg-1">
+			EN
+		</div>
+
+		<div style="text-align: right;" class="col-lg-6">
+			7:301m - 4:30pm
+		</div>
+
+		<div  class="col-lg-2">
+			0999298338
+		</div>
+
+		<div  class="col-lg-2">
+			sudanit2015@gmail.com
+		</div>
+	</div>
+	<hr style="color: gray;background-color: gray;" />
+</div>
+
+<div id="nav">
+
+<div class="row">
+
+<div  class="col-lg-2">
+إستعلام وإستفسار
+</div>
+
+<div  class="col-lg-1">
+
+</div>
+<div  class="col-lg-1">
+
+</div>
+
+<div  class="col-lg-1">
+اتصل بنا
+</div>
+
+<div  class="col-lg-1">
+الاخبار
+</div>
+
+<div  class="col-lg-1">
+مجالاتنا
+</div>
+
+<div  class="col-lg-1">
+خدماتنا
+</div>
+
+<div  class="col-lg-1">
+من نحن
+</div>
+
+
+<div  class="col-lg-1">
+الرئيسية
+</div>
+
+<div  class="col-lg-2" style="background-color: #fff;">
+<img src="images/logo/logo.jpg" width="100">
+</div>
+
+
+
+
+</div>
+
+</div>
+
+</header>
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+	<title> موقع ايكيوبيشن الجديد </title>
+</head>
+<body>
+
+<?php include 'header.php'; ?>
+
+<section id="hero">
+	<br/> <br/> <br/>
+	<h1> مرحبا بكم في منصة إيكوبيشن <br/> الإلكترونية </h1>
+	
+</section>
+
+</body>
+</html>
